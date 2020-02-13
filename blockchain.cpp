700e4717f5ae5839727b75c2202fb850a87425bf
Kay Webber <mikayla.webber@lmco.com>
2020-02-13 18:41:08 -0500

    Added add block function.

--- blockchain.cpp
+++ blockchain.cpp
@@ -1,10 +1,12 @@
 #include "blockchain.h"
 
+// constructor 
 Blockchain::Blockchain() {
     chain.emplace_back(Block(0, "Genesis Block"));
     difficulty = 6;
 }
 
+// adding a block to the blockchain 
 void Blockchain::AddBlock(Block newBlock) {
     newBlock.previousHash = GetLastBlock().GetHash();
     newBlock.MineBlock(difficulty);
